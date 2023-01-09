def95a4211c1a9589ed6485114e715c47ecbd627
kt47509 <krystafurtoyota@gmail.com>
2023-01-09 17:29:55 -0500

    initial commit

---
+++ src/utils/constants.js
@@ -0,0 +1,26 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon />, },
+  { name: 'YaleCourses', icon: <SchoolIcon />, },
+  { name: 'London Review of Books', icon: <SchoolIcon />, },
+  { name: 'TraversyMedia', icon: <CodeIcon />, },
+  { name: 'developedbyed', icon: <CodeIcon />, },
+  { name: 'GillJazzTranscriptions', icon: <MusicNoteIcon />, },
+  { name: 'David Bennett Thomas', icon: <MusicNoteIcon /> },
+  { name: 'GYPSYandJAZZ', icon: <MusicNoteIcon />, },
+  { name: 'Master Daniel Lee', icon: <FitnessCenterIcon/>, },
+  { name: 'Yoga With Adriene', icon: <FitnessCenterIcon />, },
+  { name: 'NFL', icon: <LiveTvIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://i.ytimg.com/vi/8WLVkyf2Ojg/hqdefault.jpg';
+export const demoChannelUrl = '/channel/UCFKE7WVJfvaHW5q283SxchA';
+export const demoVideoUrl = '/video/h6D7MGLs4Po';
+export const demoChannelTitle = 'Demo Channel Title';
+export const demoVideoTitle = 'Demo Video Title';
